056b8cc551ddf240dfdd5ffa0fba736211766ed8
baillema <mathieu.baille@gmail.com>
2019-09-08 15:23:25 +0200

    Update gitignore with spring config + Add init source project

---
+++ src/main/java/com/mist/insterval/InstervalApplication.java
@@ -0,0 +1,13 @@
+package com.mist.insterval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstervalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstervalApplication.class, args);
+	}
+
+}
